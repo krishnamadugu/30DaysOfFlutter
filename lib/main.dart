332d9d04a180a82a66f1ad08a78ba1c26a58bbcc
krishnamadugu <krishnamusalimadugu@gmail.com>
2023-03-09 10:58:34 +0530

    day 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:catalog/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,12 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Welcome to catalog"),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+      },
     );
   }
 }
